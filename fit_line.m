be1d0114541c4025274fa46a62be049a280c2ee9
Jan Adelmann <jan.adelmann@gmail.com>
2021-05-25 14:33:11 +0200

    added nlmfit functionality

--- fit_line.m
+++ fit_line.m
@@ -1,10 +1,14 @@
 function fit_line
 
+% read the data 
 T_diff = readtable('Parameter_tables/Mean_Diameter_change_CV_Diff.csv');
 T_all = readtable('Parameter_tables/Mean_Diameter_change_vs_CV_all.csv');
 T_p = readtable('Parameter_tables/Mean_Diameter_change_vs_CV_p.csv');
 T_d = readtable('Parameter_tables/Mean_Diameter_change_vs_CV_d.csv');
 
+%=========================================================================%
+% model using polyfit()
+%=========================================================================%
 
 % get slope for CV_lambda 
 
@@ -16,7 +20,6 @@ k = CV_diff_slope(2)
 CV_all_slope = linearfit(log(T_all.diameter_lambda), log(T_all.CV_lambda))
 
 sub_p_cv_lambda = T_p.CV_lambda(1:10)
-
 
 CV_p_slope = linearfit(log(T_p.diameter_lambda(1:10)), log(T_p.CV_lambda(1:10)))
 
@@ -31,6 +34,36 @@ CV_d_slope_c0 = linearfit(log(T_d.diameter_lambda), log(T_d.CV_0))
 function f = linearfit(x, y)
     f = polyfit(x,y,1);
 end
+
+%=========================================================================%
+% fit nlm instead of polyfit 
+%=========================================================================%
+
+% set up the model 
+beta0 = [0,0];
+linear_function = @(b,x)(b(1)*x + b(2));
+
+% model for diffusion coeff. 
+mdl_diff = fitnlm(log(T_diff.diameter_lambda), log(T_diff.CV_lambda), linear_function, beta0)
+
+%model for degradation d 
+mdl_d = fitnlm(log(T_d.diameter_lambda), log(T_d.CV_lambda), linear_function, beta0)
+
+% model for production p 
+mdl_p = fitnlm(log(T_p.diameter_lambda), log(T_p.CV_lambda), linear_function, beta0)
+
+% model with all parameters
+mdl_all = fitnlm(log(T_all.diameter_lambda), log(T_all.CV_lambda), linear_function, beta0)
+
+% fit the same models for CV_0
+mdl_all_cv0 = fitnlm(log(T_all.diameter_lambda), log(T_all.CV_0), linear_function, beta0)
+mdl_p_cv0 = fitnlm(log(T_p.diameter_lambda), log(T_p.CV_0), linear_function, beta0)
+mdl_diff_cv0 = fitnlm(log(T_diff.diameter_lambda), log(T_diff.CV_0), linear_function, beta0)
+mdl_d_cv0 = fitnlm(log(T_d.diameter_lambda), log(T_d.CV_0), linear_function, beta0)
+
+%=========================================================================%
+% plot some data to check
+%=========================================================================%
 
 x= linspace(0.01,10, 1000);
 plot(T_diff.diameter_lambda, T_diff.CV_lambda)
